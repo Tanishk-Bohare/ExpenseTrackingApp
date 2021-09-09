599f9a77c2a1ff62a0d5a91631fe060db8b47121
BABA <tanishkbohare.met18@itbhu.ac.in>
2021-09-09 12:59:12 +0530

    init

---
+++ client/src/Components/IncomeExpenses.js
@@ -0,0 +1,32 @@
+import { useContext } from 'react'
+import { GlobalContext } from '../Context/GlobalState'
+import numberWithCommas from '../utils/format'
+
+const IncomeExpenses = () => {
+    const {transactions} = useContext(GlobalContext)
+
+    const amounts = transactions.map((transaction)=> transaction.amount)
+    const income = amounts
+                    .filter(amount =>  amount>0)
+                    .reduce((acc, amount) => (acc+amount), 0)
+                    .toFixed(2)
+    const expense = amounts
+                    .filter(amount =>  amount<0)
+                    .reduce((acc, amount) => (acc+amount), 0)
+                    .toFixed(2)
+
+    return (
+        <div className="inc-exp-container" >
+            <div >
+                <h4>Income</h4>
+                <p className="money plus" >+${numberWithCommas(income)}</p>
+            </div>
+            <div>
+                <h4>Expense</h4>
+                <p className="money minus" >-${numberWithCommas(expense)}</p>
+            </div>
+        </div>
+    )
+}
+
+export default IncomeExpenses
---
+++ client/src/Components/Transaction.js
@@ -0,0 +1,18 @@
+import { useContext } from 'react'
+import { GlobalContext } from '../Context/GlobalState'
+import numberWithCommas from '../utils/format'
+
+const Transaction = ({transaction}) => {
+    const { deleteTransaction } = useContext(GlobalContext)
+    
+    const sign = transaction.amount < 0 ? '-': '+'
+    
+    return (
+        <li className={transaction.amount<0?'minus':'plus'} >
+            {transaction.text} <span>{sign}${numberWithCommas(Math.abs(transaction.amount))}</span><button 
+            className="delete-btn" onClick={()=>deleteTransaction(transaction._id)}  >x</button>
+        </li>
+    )
+}
+
+export default Transaction
